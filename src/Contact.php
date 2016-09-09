66bf0d2c4b62c6ab807483c3d64e281ed57b382d
April Peng <helloaprilpeng@gmail.com>
2016-09-09 10:32:46 -0700

    create all static and regular methods needed in class for assignment

--- src/Contact.php
+++ src/Contact.php
@@ -11,33 +11,45 @@
             $this->phone = $phone;
             $this->address = $address;
         }
-    }
 //Getters and Setters
-    function setName($new_name)
-    {
-        $this->name = (string) $new_name;
-    }
-    function getName()
-    {
-        return $this->name;
-    }
-    function setPhone($new_phone)
-    {
-        $this->phone = (int) $new_phone;
-    }
-    function getPhone()
-    {
-        return $this->phone;
-    }
-    function setAddress($new_address)
-    {
-        $this->address = $new_address;
-    }
-    function getAddress()
-    {
-        return $this->address;
-    }
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+        function getName()
+        {
+            return $this->name;
+        }
+        function setPhone($new_phone)
+        {
+            $this->phone = (int) $new_phone;
+        }
+        function getPhone()
+        {
+            return $this->phone;
+        }
+        function setAddress($new_address)
+        {
+            $this->address = $new_address;
+        }
+        function getAddress()
+        {
+            return $this->address;
+        }
 //Methods
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
 
 //Static Methods
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+        static function deleteAll()
+        {
+            $_SESSION['list_of_contacts'] = array();
+        }
+    }
 ?>
