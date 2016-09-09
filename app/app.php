eee9a793c2667c57af71a7eb1bc7f4f80a60edf6
April Peng <helloaprilpeng@gmail.com>
2016-09-09 10:34:51 -0700

    add initial ref to get homepage. Somethings not working error

--- app/app.php
+++ app/app.php
@@ -1,14 +1,19 @@
 <?php
-    require_once __DIR__.'/../vendor/index.php';
+    require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/Contact.php';
 
     session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
 
     $app = new Silex\Application();
 
-    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views');
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('contacts.twig.html', array('contacts') => Contact::getAll());
+    });
 
     return $app;
 ?>
